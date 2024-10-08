f1fa8cd6fd0f19a12d0004550afe0c8ec8fdb137
MeritonAliu <tonialiu03@gmail.com>
2024-10-08 23:32:31 +0200

    remove constructor

--- src/EmojiLoader.cs
+++ src/EmojiLoader.cs
@@ -4,11 +4,6 @@ internal static class EmojiLoader
 {
     private static readonly HttpClient httpClient = new HttpClient();
     private static readonly string url = "https://raw.githubusercontent.com/muan/unicode-emoji-json/refs/heads/main/data-by-emoji.json";
-    static EmojiLoader()
-    {
-
-    }
-
     internal static string DownloadJsonFromUrl()
     {
         string localFilePath = Path.Combine(AppContext.BaseDirectory, "downloaded-data-by-emoji.json");
